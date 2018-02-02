d81d23117a0e9a83ffc9e140ebc99f5bb611262c
Denys Nahurnyi <denysnahyrnui@gmail.com>
2018-02-02 19:38:37 +0200

    clean code from comments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # find-places-around
+This is repository of project, that uses aws lambda service to implement serverless architecture.
--- handler.js
+++ handler.js
@@ -14,29 +14,14 @@ module.exports.hello = (event, context, callback) => {
 
 module.exports.test = (event, context, callback) => {
   let fs = require('fs')
-  // // let _ = require('underscore')
-  // console.log('1')
-  // // let v1 = _.map([1, 2, 3], function(num){ return num * 3; })
-  // console.log('2')
+
   fs.writeFileSync('/tmp/message.txt', 'Hello Node.js');
-  console.log('3')
-
   let data = fs.readFileSync('/tmp/message.txt');
-  console.log('4')
-  // // fs.unlinkSync('/tmp/message.txt');
-
   let response = {
     statusCode: 200,
     headers: {'Content-type' : 'text/plain'},
-    body: data.toString('utf8'),
-    isBase64Encoded : true,
+    body: data.toString('utf8')
   };
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'v1.0'
-  //   }),
-  // };
 
   callback(null, response);
 };
